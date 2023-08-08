7c54054f3be7efeb6cb81ac25fca10a008f9e80b
KUNAL <goudanikunal3@gmail.com>
2023-08-09 02:43:43 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import Hero from '../src/components/hero/Hero'
+import Header from './components/header/Header';
+import NavMobile from './components/nav/NavMobile';
+import Stats from './components/stats/Stats';
+import Why from './components/stats/Why';
+import Trade from './components/sections/Trade';
+
+import { useEffect, useState } from 'react';
+import Calculate from './components/sections/Calculate';
+import Features from './components/sections/Features';
+import NewsLetter from './components/sections/NewsLetter';
+import Footer from './components/sections/Footer';
+function App() {
+  //state management
+  const [navMobile, setNavMobile] = useState(false);
+  //aos init
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  }
+  );
+
+  return (
+    <div className='overflow-hidden'>
+      {/* <Header setNavMobile={setNavMobile} /> */}
+      <Hero />
+      {/* Mobile Nav */}
+      {/* <div className={`${navMobile ? 'right-0' : '-right-full'} fixed z-10 top-0 h-full transition-all duration-200`}>
+        <NavMobile setNavMobile={setNavMobile} />
+      </div> */}
+      <Stats />
+      <Why />
+      <Calculate />
+      <Trade />
+      <Features />
+      <NewsLetter />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "../../assets/img/hero-img.png";
+// import icons
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+
+const Hero = () => {
+  return (
+    <section className="container mx-auto">
+      <div className="flex flex-col items-center lg:flex-row">
+        {/* Hero text */}
+        <div className="flex-1">
+          {/* badge test */}
+          <div
+            className="bg-slate-200 p-1 mb-6 rounded-full pl-1 pr-3 max-w-[365px]"
+            data-aos="fade-down"
+            data-aos-delay="400"
+          ></div>
+          {/* title */}
+          <h1
+            className="text-[32px] lg:text-[64px] leading-tight mb-6 "
+            data-aos="fade-down"
+            data-aos-delay="500"
+          >
+            Fastest & secure platform to invest in crypto.
+          </h1>
+          <p
+            className="max-w-[440px] leading-relaxed mb-8"
+            data-aos="fade-down"
+            data-aos-delay="600"
+          >
+            Buy and Sell cryptocurrency, trusted by 10M wallets with over $30
+            billion in transactions.
+          </p>
+          <button
+            className="bg-blue-500  px-6 py-2 rounded-full text-white text-sm lg:h-16 lg:text-base font-bold gap-x-6"
+            data-aos="fade-down"
+            data-aos-delay="700"
+          >
+            <div className="flex items-center justify-betweeen gap-2">
+              Upload
+              <IoIosArrowDroprightCircle className="text-2xl lg:text-3xl" />
+            </div>
+          </button>
+        </div>
+        {/* Hero image flex-col2nd element */}
+        <div className="flex-1">
+          <img
+            src={Image}
+            alt="banner"
+            data-aos="fade-up"
+            data-aos-delay="600"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
